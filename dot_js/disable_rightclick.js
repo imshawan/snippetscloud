5490548f071d46e2e9b44ce19d7ffeae2e0bd909
Shawan Mandal <shawan.sm049@gmail.com>
2022-01-07 22:42:14 +0530

    Disable right click

---
+++ dot_js/disable_rightclick.js
@@ -0,0 +1,23 @@
+/**
+ * @author Shawan Mandal
+ * @description Disable ctrl + shift + i and right click for a web page using JavaScript
+ */
+
+document.addEventListener('contextmenu', event => event.preventDefault()); //Prevent from Right Click
+document.onkeydown = function prevent_InspectElement(e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
